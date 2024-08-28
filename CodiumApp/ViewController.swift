bdd47ee3d3a2b9209051584e1bd2c8d73719cf25
aiful-yohei-shimizu <yohei.shimizu.aiful@gmail.com>
2024-08-28 19:25:24 +0900

    ランダムで1~10の整数値を返却し返却値が10以上だったらエラーToastを表示する処理

--- CodiumApp/ViewController.swift
+++ CodiumApp/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // viewDidApperでprintRandomValueメソッドを実行する
+    override func viewDidAppear(_ animated: Bool) {
+        printRandomValue()
+    }
 
+    // ランダムで1~10の整数値を返却し返却値が10以上だったらエラーをthrowするメソッド
+    func randomValue() throws -> Int {
+        // 1~10の整数値をランダムに生成
+        let randomValue = Int.random(in: 1...10)
+        if randomValue >= 10 {
+            throw NSError(domain: "randomValue", code: 0, userInfo: nil)
+        }
+        return randomValue
+    }
+
+    // randomValueをdo-catch文で実行し、doの場合は結果をprint、catchの場合はエラーアラートを表示する。
+    func printRandomValue() {
+        do {
+            let result = try randomValue()
+            print(result)
+        } catch {
+            let alert = UIAlertController(title: "エラー", message: "ランダムの数字が10を超えました。", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
 }
 
